0fc937ec748af504776d5d0f58f33dff1dd75884
Lucassml-boop <lucas.samuel23borges@gmail.com>
2025-02-07 09:45:37 -0300

    Implementando a animações na transição

--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -1,13 +1,22 @@
 import React from "react";
 import styles from "../Styles/TodoItem.module.css";
+import {motion} from "framer-motion";
 
 function TodoItem({todo, removeTodo, toggleComplete}){
     return(
-        <li className={styles.item}>
-           <span className={todo.completed ? styles.completed : ""}>{todo.text}</span> 
-            <button className={styles.button} onClick={() => toggleComplete(todo.id)}>✅</button>
-            <button className={styles.button} onClick={() => removeTodo(todo.id)}>❌</button>
-        </li>
+        <motion.li
+            className={styles.item}
+            initial={{opacity: 0, y: -20}}
+            animate={{opacity: 1, y: 0}}
+            exit={{opacity: 0, x: -50}}
+            transition={{duration: 0.8}}
+        >
+        <span className={todo.completed ? styles.completed : ""}>{todo.text}</span> 
+        <div>
+            <button className={styles.button_green} onClick={() => toggleComplete(todo.id)}>✅</button>
+            <button className={styles.button_red} onClick={() => removeTodo(todo.id)}>❌</button>
+        </div>
+        </motion.li>
     );
 }
 
